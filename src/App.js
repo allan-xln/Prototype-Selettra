952ba917c975d6005bbf653765d9911cc128d98b
Allan da Silva Pereira <allan.pereira@cotrasa.local>
2023-05-23 23:47:01 -0300

    prototypeSeletra

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+import './homepage.css';
+import './buttons.css';
+import './index.js';
+import './script.js';
+
+function App() {
+
+  return (
+    <div className="container">
+    </div>
+
+  );
+}
+
+export default App;
